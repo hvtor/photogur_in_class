64bac73c1be93ca483b245887bda0d0ee6ca1e6d
Monica Olinescu <monica.olinescu@gmail.com>
2013-03-19 14:18:00 -0400

    Creating the pictures controller with the index action

---
+++ app/controllers/pictures_controller.rb
@@ -0,0 +1,6 @@
+class PicturesController < ActionController::Base
+
+  def index
+    
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PhotogurInClass::Application.config.secret_token = '6a52e04984a6159060f185ee327bdb7db0c93a5ee489e8841662b78343c26e624ba5ae7290c7f4a09789c39e4cb7dd3ecb88fd27133c0acb1e3723f286560e57'
