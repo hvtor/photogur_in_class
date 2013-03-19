fd243fce340031423cd183e0ff3e9cf133a8ae31
Monica Olinescu <monica.olinescu@gmail.com>
2013-03-19 15:42:38 -0400

    Adding the detailed image information to github

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -4,10 +4,22 @@ class PicturesController < ApplicationController# NOT: ActionController::Base
     @greeting = "Hello World"
 
     @pictures = [
-      'http://monicao.s3.amazonaws.com/bitmaker/house.jpg', 
-      'http://monicao.s3.amazonaws.com/bitmaker/wave.jpg', 
-      'http://monicao.s3.amazonaws.com/bitmaker/girl.jpg',
-      'http://lolcat.com/images/lolcats/1338.jpg'
+      {
+        :title  => "The old church on the coast of White sea",
+        :artist => "Sergey Ershov",
+        :url    => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
+      },
+      {
+        :title  => "Sea Power",
+        :artist => "Stephen Scullion",
+        :url    => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"
+      },
+      {
+        :title  => "Into the Poppies",
+        :artist => "John Wilhelm",
+        :url    => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
+      }
     ]
+    @title = "This is my title"
   end
 end
