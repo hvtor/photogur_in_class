ea9fa4c02dbec1ef1e53e059ab22f1be453050df
Monica Olinescu <monica.olinescu@gmail.com>
2013-03-19 14:49:00 -0400

    Displaying picture urls from the array

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,6 +1,13 @@
-class PicturesController < ActionController::Base
+class PicturesController < ApplicationController# NOT: ActionController::Base
 
   def index
-    
+    @greeting = "Hello World"
+
+    @pictures = [
+      'http://monicao.s3.amazonaws.com/bitmaker/house.jpg', 
+      'http://monicao.s3.amazonaws.com/bitmaker/wave.jpg', 
+      'http://monicao.s3.amazonaws.com/bitmaker/girl.jpg',
+      'http://lolcat.com/images/lolcats/1338.jpg'
+    ]
   end
 end
