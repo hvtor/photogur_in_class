614d6f9e58aeab840cb0ac98ef50c474f510c80d
Khurram Virani <kvirani@gmail.com>
2013-03-20 11:37:56 -0400

    First use of the database in our PictuersController. Insert and Load data from the DB

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,39 +1,34 @@
 class PicturesController < ApplicationController# NOT: ActionController::Base
-  before_filter :load_pictures
 
   def index
+    @pictures = Picture.all
   end
 
   def show
-    @picture = @pictures[params[:id].to_i]
+    # params = {
+    #   :id => "5"
+    # }
+    @picture = P.find params[:id]
   end
 
   def new
   end
 
   def create
-    render :text => "Saving a picture. Url: #{params[:url]}, Title: #{params[:title]}, Artist: #{params[:artist]}"
+    # params = {
+    #   :url => "http://fdasfdsafds",
+    #   :title => "some title",
+    #   :artist => "some artist"
+    # }
+    @picture = Picture.new
+    @picture.url = params[:url]
+    @picture.title = params[:title]
+    @picture.artist = params[:artist]
+    success = @picture.save
+    if success
+      redirect_to '/pictures' #pictures_path
+    end
+    #render :text => "Saving a picture. Url: #{params[:url]}, Title: #{params[:title]}, Artist: #{params[:artist]}"
   end
-
-  def load_pictures
-    @pictures = [
-      {
-        :title  => "The old church on the coast of White sea",
-        :artist => "Sergey Ershov",
-        :url    => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
-      },
-      {
-        :title  => "Sea Power",
-        :artist => "Stephen Scullion",
-        :url    => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"
-      },
-      {
-        :title  => "Into the Poppies",
-        :artist => "John Wilhelm",
-        :url    => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
-      }
-    ]
-  end
-
 
 end
