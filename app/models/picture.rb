3191cfe04863754447dc88bab7b4de92fb588a98
Monica Olinescu <monica.olinescu@gmail.com>
2013-03-21 15:14:53 -0400

    Cleaning up the code a bit

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,4 +2,6 @@ class Picture < ActiveRecord::Base
   # Allow mass-assignment for the attributes title and artist
   attr_accessible :title, :artist, :url
   # Don't confuse this with attr_accessor
+
+  validate :url, :presence => true
 end
